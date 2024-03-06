660d55f44a04c379d79d0438e182bfe22960dd6e
malick <mkdiallo@bis-bank.sn>
2024-03-06 07:20:51 +0000

    use .env file for database connections

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const path = require('path');
 
 
 const app = express();
-const port = 3000;
+//const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
