3624d7a95638b5c5584d00eb79433f66bb3576d4
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2024-03-09 17:45:01 +0000

    last edit

--- middlewares/paydunya/PAYDUNYA_CASHIN.js
+++ middlewares/paydunya/PAYDUNYA_CASHIN.js
@@ -7,10 +7,15 @@ const paydunyaCashIn = async (disburse_invoice, disburse_id) => {
             "disburse_invoice": disburse_invoice,
             "disburse_id": disburse_id ?? ""
         };
-        const res = await axios.post(ENV_CONTENTS.PAYDUNYA_CASHIN_BASE_URL + 'disburse/submit-invoice', payloads);
+        var headers = {
+            'PAYDUNYA-MASTER-KEY': ENV_CONTENTS.PAYDUNYA_MASTER_KEY,
+            'PAYDUNYA-PRIVATE-KEY': ENV_CONTENTS.PAYDUNYA_PRIVATE_KEY,
+            'PAYDUNYA-TOKEN': ENV_CONTENTS.PAYDUNYA_TOKEN
+        };
+        const res = await axios.post(ENV_CONTENTS.PAYDUNYA_CASHIN_BASE_URL + 'disburse/submit-invoice', payloads, { headers: headers });
         return res.data;
     } catch (error) {
-        throw new Error(JSON.stringify(error?.response?.data?? {"msg": error.message}));
+        throw new Error(JSON.stringify(error?.response?.data ?? { "msg": error.message }));
     }
 
 }
